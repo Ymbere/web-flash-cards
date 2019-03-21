1643164af51e903e5660648b8c78c51c7cd0d262
Ymbere <ymbere@gmail.com>
2019-03-20 23:23:11 -0300

    Add new methods for the API

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,8 +2,19 @@
 const meuStorage = "meuStorage"
 
 export function retriveDecks() {
+    const stateCheck = JSON.parse(localStorage.getItem(meuStorage))
+
+    if (stateCheck === null) {
+        localStorage.setItem(meuStorage, JSON.stringify([]))
+    }
+
     const decks = localStorage.getItem(meuStorage)
     return JSON.parse(decks)
+}
+
+export function retriveOneDeck(deck_id) {
+    const decks = retriveDecks()
+    return decks.find(deck => deck.id.toString() === deck_id)
 }
 
 export function addDeckToStorage(deck) {
@@ -11,11 +22,25 @@ export function addDeckToStorage(deck) {
     localStorage.setItem(meuStorage, JSON.stringify(newState))
 }
 
+export function addCardToStorage(card) {
+    const newState = addCardToDeck(card)
+    localStorage.setItem(meuStorage, JSON.stringify(newState))
+}
+
+function addCardToDeck(card) {
+    const state = retriveDecks()
+    return state.map((deck) => {
+        if (deck.id === card.deck_id) {
+            return {
+                ...deck,
+                cards: [...deck.cards, card]
+            }
+        }
+        return deck
+    })
+}
+
 function addItem(item) {
-    const stateCheck = retriveDecks()
-    if (stateCheck === null) {
-        localStorage.setItem(meuStorage, JSON.stringify([]))
-    }
     const state = retriveDecks()
     state.push(item)
     return state
