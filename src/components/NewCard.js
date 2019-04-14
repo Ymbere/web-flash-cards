87fbb3180ce11537afb1930ce328b31b05327b24
Ymbere Xavier <ymbere@gmail.com>
2019-04-14 02:19:49 -0300

    Update NewCard
    Update New Card in order to automatically redirect to the
    deck main page when a new card is created

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -2,11 +2,26 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { handleAddCard } from '../redux/actions/DeckActions';
 import NavBar from './NavBar';
+import { Redirect } from "react-router-dom";
 
 class Newcard extends Component {
     state = {
         question: '',
-        answer: ''
+        answer: '',
+        deckID: '',
+        redirect: false
+    }
+
+    setRedirect = () => {
+        this.setState({
+            redirect: true
+        })
+    }
+
+    renderRedirect = ( deckID ) => {
+        if (this.state.redirect) {
+            return <Redirect to={`/deck/${deckID}`} />
+        }
     }
 
     handleChangeInput = (e) => {
@@ -33,19 +48,37 @@ class Newcard extends Component {
             deck_id: parseInt(parentID)
         }))
 
+        this.setRedirect()
+
         this.setState(() => ({
             question: '',
             answer: ''
         }))
     }
 
+    componentDidMount() {
+        const deckID = this.props.match.params.id
+        this.setState({
+            deckID
+        })
+    }
+
     render() {
         return (
             <Fragment>
+                {this.renderRedirect(this.state.deckID)}
                 <NavBar />
                 <form onSubmit={this.handleSubmit}>
-                    <input id="question" placeholder="Digite a pergunta do card" onChange={this.handleChangeInput} value={this.state.question} /><br/>
-                    <input id="answer" placeholder="Digite a resposta para o card" onChange={this.handleChangeInput} value={this.state.answer} />
+                    <input id="question"
+                        placeholder="Digite a pergunta do card"
+                        onChange={this.handleChangeInput}
+                        value={this.state.question}
+                    /><br/>
+                    <input id="answer"
+                        placeholder="Digite a resposta para o card"
+                        onChange={this.handleChangeInput}
+                        value={this.state.answer}
+                    />
                     <br/>
                     <button>Submit</button>
                 </form>
