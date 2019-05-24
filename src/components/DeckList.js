1b834b2ad126fcbec4a1f3a67894237d2be9425a
Ymbere <ymbere@gmail.com>
2019-05-24 18:47:21 +0000

    Update deck list
    Render a componente when the deck list is empty

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -3,6 +3,52 @@ import { connect } from 'react-redux'
 import DeckCard from './DeckCard';
 import { handleInitialData } from '../redux/actions/Shared';
 import NavBar from './NavBar';
+import EmptyDeck from './EmptyDecks';
+
+
+function RenderEmptyScreen() {
+    return (
+        <EmptyDeck />
+    )
+}
+
+function RenderDeckList(props) {
+    const decks = props.decks
+
+    return (
+        <>
+            {decks !== null &&
+                decks.map((deck) => (
+                    <DeckCard
+                        key={deck.id}
+                        id={deck.id}
+                    />
+                ))
+            }
+        </>
+    )
+}
+
+
+function RenderScreen(props) {
+    const decks = props.decks
+
+    if (decks.length === 0) {
+        return (
+            <>
+                <NavBar />
+                <RenderEmptyScreen />
+            </>
+        )
+    } else {
+        return (
+            <>
+                <NavBar />
+                <RenderDeckList decks={decks} />
+            </>
+        )
+    }
+}
 
 class DeckList extends Component {
     componentDidMount() {
@@ -13,17 +59,7 @@ class DeckList extends Component {
         const { decks } = this.props
 
         return (
-            <div>
-                <NavBar />
-                {decks !== null &&
-                    decks.map((deck) => (
-                        <DeckCard
-                            key={deck.id}
-                            id={deck.id}
-                        />
-                    ))
-                }
-            </div>
+            <RenderScreen decks={decks} />
         )
     }
 }
