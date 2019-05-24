9bb7e71e4c3ba1406b3d0ba98dd1c45ad9a4de90
Ymbere <ymbere@gmail.com>
2019-05-24 18:34:30 +0000

    Add new component
    Add component to be displayed when the deck list
    is empty

---
+++ src/components/EmptyDecks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class EmptyDeck extends Component {
+    render() {
+        return(
+            <div>
+                <h6>You don't have any decks, you should try add some</h6>
+            </div>
+        )
+    }
+}
+
+export default EmptyDeck
